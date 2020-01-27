e7b64d612603f7232c6fdd03768c5f84a560dd7e
gcf <gwenael.cesar-felllmann@meteo.fr>
2020-01-27 17:13:23 +0100

    cdp wave

--- cdp/views.py
+++ cdp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import cdp.models
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World from django app CdP.")
+	if request.method == 'POST':
+		dp = cdp.models.CdpApic(request.files['file'])
+		return HttpResponse(f"{dp.name} reçu à {dp.dt} avec {dt.grains}")	
+
+    return HttpResponse("Hello World from django app CDP (I want a post, please gimme a post)")
