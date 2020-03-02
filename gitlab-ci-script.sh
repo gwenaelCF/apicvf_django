71ca9fc5e724b2527e9a32ec404cfd495464f9d8
MARRAST MATTHIEU <matthieu.marrast@meteo.fr>
2020-03-02 14:04:31 +0000

    Update gitlab-ci-script.sh

--- gitlab-ci-script.sh
+++ gitlab-ci-script.sh
@@ -25,6 +25,13 @@ testu()
     # install plugin
     install
 
+    # edit config
+    cd ${PLUGIN_RUNTIME}/${PLUGIN_NAME}
+    sed -i "s/'HOST':.*/'HOST': 'postgres',/" settings.py
+    sed -i "s/'PORT':.*/'PORT': '5432',/" settings.py
+    sleep 10
+    
+    # launch tests+coverage
     cd ${PLUGIN_RUNTIME}
     echo "-> A. Launch test (through coverage)"
     plugin_wrapper ${PLUGIN_NAME} coverage run manage.py test carto.tests
