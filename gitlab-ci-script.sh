d1040792d00d639635a0a924f223eef544c43bd4
MARRAST MATTHIEU <matthieu.marrast@meteo.fr>
2020-03-02 13:28:23 +0000

    Update gitlab-ci-script.sh

--- gitlab-ci-script.sh
+++ gitlab-ci-script.sh
@@ -10,19 +10,25 @@ PLUGIN_RUNTIME="/home/mfserv/var/plugins/${PLUGIN_NAME}"
 install()
 {
     # start mfserv
+    echo "-> 1. Start mfsev"
     mfserv.start
 	# make relase
+	echo "-> 2.Make release"
 	make release
 	# install plugin
+	echo "-> 3. Install plugin"
 	plugins.install apicvf_django-*.metwork.mfserv.plugin
 }
 
 testu()
 {
+    # install plugin
     install
 
     cd ${PLUGIN_RUNTIME}
-    coverage run --source='carto' manage.py test carto.tests --keepdb
+    echo "-> A. Launch test (through coverage)"
+    plugin_wrapper ${PLUGIN_NAME} coverage run manage.py test carto.tests
+    echo "-> B. Launch coverage xml report"
     coverage xml
 }
 
