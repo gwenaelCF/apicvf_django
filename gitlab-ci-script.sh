b80f115e558e1c8bc6453cbe072d87a70d7ee1e7
MARRAST MATTHIEU <matthieu.marrast@meteo.fr>
2020-03-03 08:41:17 +0000

    Update gitlab-ci-script.sh

--- gitlab-ci-script.sh
+++ gitlab-ci-script.sh
@@ -2,19 +2,29 @@
 # gitlab-ci-script.sh [options]
 # by default run testu
 # with --testu, run only the testu
+# with --sonar, run only the sonar
+# with --build, run only the build
 
 GIT_BASE="/builds/apic/"
 PLUGIN_NAME="apicvf_django"
 PLUGIN_RUNTIME="/home/mfserv/var/plugins/${PLUGIN_NAME}"
 
-install()
+prepare()
 {
     # start mfserv
     echo "-> 1. Start mfsev"
     mfserv.start
-	# make relase
+}
+
+make_release()
+{
+    # make relase
 	echo "-> 2.Make release"
 	make release
+}
+
+install()
+{
 	# install plugin
 	echo "-> 3. Install plugin"
 	plugins.install apicvf_django-*.metwork.mfserv.plugin
@@ -22,6 +32,10 @@ install()
 
 testu()
 {
+    # start mfserv
+    prepare
+    # make release
+    make_release
     # install plugin
     install
 
@@ -31,7 +45,7 @@ testu()
     sed -i "s/'PORT':.*/'PORT': '5432',/" settings.py
     sleep 10
     
-    # launch tests+coverage
+    # launch tests + coverage
     cd ${PLUGIN_RUNTIME}
     echo "-> A. Launch test (through coverage)"
     plugin_wrapper ${PLUGIN_NAME} coverage run manage.py test carto.tests
@@ -45,13 +59,13 @@ sonar()
     # move to plugin installed directory
     cd ${PLUGIN_RUNTIME}
     
-    # change the path of the sources to match the sources of the project
+    # coverage - change the path of the sources to match the sources of the project
     sed -i "s#/home/mfserv/var/plugins/${PLUGIN_NAME}/##g" coverage.xml
     
     # launch sonar-scanner
     sonar-scanner
     
-    # extract result
+    # extract sonar result
     export url=$(cat .scannerwork/report-task.txt | grep ceTaskUrl | cut -c11-)
     sleep 5s #Wait time for the report
     curl -k $url -o analysis.txt #store results in analysis.txt
@@ -60,7 +74,14 @@ sonar()
     curl -k  http://sonar.meteo.fr/api/qualitygates/project_status?analysisId=$analysisId -o result.txt;
     export result=$(cat result.txt | jq -r '.projectStatus.status');
     if [ “$result” == “ERROR” ]; then echo -e 'SONAR RESULTS FAILED'; exit 1; fi
+}
 
+build()
+{
+    # start mfserv
+    prepare
+    # make release
+    make_release
 }
 
 cd ${GIT_BASE}/${PLUGIN_NAME}/
@@ -71,6 +92,9 @@ case "$1" in
     ;;
 "--sonar")
     sonar
+    ;;
+"--build")
+    build
     ;;
 *)
 	testu
